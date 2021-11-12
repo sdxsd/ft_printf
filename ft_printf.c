38c3bc3d1507f38d24484022d1eb7617cc594d84
sdxsd <willmaguire@icloud.com>
2021-11-12 16:34:18 +0100

    Beginning printf

---
+++ ft_printf.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/11 15:29:03 by wmaguire      #+#    #+#                 */
+/*   Updated: 2021/11/12 15:48:13 by wmaguire      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+int	ft_printf(const char *fmt, ...)
+{
+	int	iterator;
+
+	while (fmt[iterator] != '\0')
+	{
+		if (fmt[iterator] == '%')
+			ft_putchar
+	}
+}
