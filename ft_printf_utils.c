f0fc4b94f3f26f0291128db53f1d429eda44eed7
sdxsd <willmaguire@icloud.com>
2021-11-18 13:44:26 +0100

    Removed norminette error

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 12:07:06 by wmaguire          #+#    #+#             */
-/*   Updated: 2021/11/18 13:11:45 by wmaguire         ###   ########.fr       */
+/*   Updated: 2021/11/18 13:44:09 by wmaguire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,33 +36,6 @@ int	ft_puthex(size_t nb, unsigned int mode)
 	return (x);
 }
 
-/*
-int	ft_puthex(size_t nb, unsigned int mode)
-{
-	size_t	temp;
-	int		x;
-
-	x = 0;
-	if (nb == 0)
-		return (write(1, "0", 1));
-	while (nb != 0)
-	{
-		temp = nb % 16;
-		if (temp < 10)
-			x += ft_putchar_len(temp + 48);
-		else
-		{
-			if (mode)
-				x += ft_putchar_len(temp + 55);
-			else
-				x += ft_putchar_len(ft_tolower(temp + 55));
-		}
-		nb /= 16;
-	}
-	return (x);
-}
-*/
-
 int	ft_putstr_len(char *str)
 {
 	int	iterator;
@@ -91,7 +64,7 @@ int	ft_putnbr_len(int nb)
 		nb *= (-1);
 	}
 	if (nb < 10 && x != 0)
-		return (ft_putchar_len(nb + '0') + 1);		
+		return (ft_putchar_len(nb + '0') + 1);
 	if (nb < 10)
 		return (ft_putchar_len(nb + '0'));
 	x += ft_putnbr_len(nb / 10);
